19e70b9c81de3f9756fa97f364f703f658df0fa5
Maria Memollari <mariamemollari01@gmail.com>
2024-01-01 13:28:37 +0100

    Create schema,model and controller for user and posts

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postsRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost} from "./controllers/posts.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -38,11 +42,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /*Routes with files*/
-app.post("/auth/register", upload.single("picture"), register)
-
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts",verifyToken, upload.single("picture"), createPost)
 
 /*Routes */
-app.use("/auth",authRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postsRoutes);
 
 /*Mongoose SetUp */
 const PORT = process.env.PORT || 6001;
